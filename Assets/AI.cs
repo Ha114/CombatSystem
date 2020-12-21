50dc33cd5ebf7634a606a2225490f5bfb827d4ec
Ha114 <56894396+Ha114@users.noreply.github.com>
2020-12-21 21:30:30 +0100

    3
    3

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ public class AI : MonoBehaviour
     public Transform Pl;
     public float speed = 4f;
     Rigidbody rb;
+    bool damage = false;
+    public static event Action _Damage;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +23,24 @@ public class AI : MonoBehaviour
         Vector3 pos = Vector3.MoveTowards(transform.position, Pl.position, speed * Time.deltaTime);
         rb.MovePosition(pos);
         transform.LookAt(Pl);
+    }
+
+    void Atak() {
+        var dist = Vector3.Distance(transform.position, Pl.transform.position);
+        if(dist<=2 && damage == false)
+        {
+            _Damage?.Invoke();
+            damage = true;
+            Invoke("recharge", 3);
+        }
+    }
+    void recharge()
+    {
+        damage = false;
+    }
+
+    private void Update()
+    {
+        Atak();
     }
 }
--- Assets/Combat.cs
+++ Assets/Combat.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,27 +9,38 @@ public class Combat : MonoBehaviour
     private Text health;
     private int hp = 100;
     private GameObject enemy;
+    public Action _Damage;
 
 
     void Start()
     {
         //Health
         health = GameObject.FindGameObjectWithTag("Health").GetComponent<Text>();
+        enemy = GameObject.FindGameObjectWithTag("enemy");
+        AI._Damage += TakeDamage;
     }
 
     //take Damage
     private void OnTriggerEnter(Collider other)
     {
-        hp = hp - 40;
+        //reaction to a collision with an enemy
+        Damage(2);
+    }
+    void TakeDamage() {
+        var dist = Vector3.Distance(transform.position, enemy.transform.position);
+        if (dist<=2) {
+            Debug.Log("DAMAGE");
+            Damage(40); }
+    }
+    void Damage(int d) {
+        hp = hp - d;
         if (hp <= 0) { health.text = "GAME OVER, hp = " + hp; }
         else
         {
             health.text = "Health: " + hp;
-           // other.gameObject.SetActive(false);
         }
+
     }
-
-
 
     //deal damage
     void DealDamage()
@@ -42,9 +54,8 @@ public class Combat : MonoBehaviour
 
     void CanInteract()
     {
-        enemy = GameObject.FindGameObjectWithTag("enemy");
         var dist = Vector3.Distance(transform.position, enemy.transform.position);
-        if (dist <= 3f)
+        if (dist <= 4f)
         {
             enemy.SetActive(false);
             Debug.Log("The enemy is destroyed");
@@ -58,6 +69,11 @@ public class Combat : MonoBehaviour
     private void Update()
     {
         DealDamage();
+    //    TakeDamage();
+    }
+    private void OnDisable()
+    {
+        AI._Damage -= TakeDamage;
     }
 
 }
